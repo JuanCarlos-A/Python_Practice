6c19c2cb25f8304c56ceb9611f1680a1b9090dfe
JuanCarlos-A <juancar2805@gmail.com>
2024-03-12 11:52:05 -0500

    Ejemplo de Listas

--- mentorship/tareas/list/example.py
+++ mentorship/tareas/list/example.py
@@ -1,9 +1,9 @@
 lista_normal = [2, 1, 3]
-lista_normal.append(6) # [1, 2, 3, 4, 5, 6]
-lista_normal.pop(0) # [2, 3, 4, 5, 6]
-lista_normal.remove(3) # [2, 4, 5, 6]
-lista_normal.insert(0, 1) # [1, 2, 4, 5, 6]
-lista_normal.sort() # [1, 2, 4, 5, 6]
+lista_normal.append(6) # [2, 1, 3, 6]
+lista_normal.pop(0) # [1, 3, 6]
+lista_normal.remove(3) # [1, 6]
+lista_normal.insert(0, 1) # [1, 1, 6]
+lista_normal.sort() # [1, 1, 6]
 
 print(lista_normal)
 
@@ -12,3 +12,5 @@ print(lista_normal)
 lista_vacia = []
 
 lista_iterable = [x for x in range(10)] # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(lista_iterable.index(0)) # 0
