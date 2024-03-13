7543deb1f97da041897d0aed0087a63a9a8e3cfb
JuanCarlos-A <juancar2805@gmail.com>
2024-03-13 08:51:32 -0500

    Format Strings

---
+++ python_course/tipos_basicos/03-format-string.py
@@ -0,0 +1,11 @@
+nombre = "Carlos"
+apellido = "Rodriguez"
+
+nombre_completo = f"{nombre} {apellido}"
+
+
+nombre_completo2 = f"{nombre[0]} {9*9}"
+
+print(nombre_completo) # Carlos Rodriguez
+
+print(nombre_completo2) # C 81
