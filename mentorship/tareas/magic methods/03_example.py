d61d22cb929eb7aeb0908b02e8ecd261c52a930a
JuanCarlos-A <juancar2805@gmail.com>
2024-03-13 13:31:01 -0500

    Magic,Dunder,Special Methods in Python

--- mentorship/tareas/magic methods/03_example.py
+++
@@ -1,7 +0,0 @@
-class Persona:
-    def __init__(self, nombre, edad): # Método para inicializar el objeto
-        self.nombre = nombre
-        self.edad = edad
-    
-p1 = Persona("Juan", 25) # Creación de un objeto de la clase Persona
-p2 = Persona("Maria", 30)
