9ff89a48c9b47d7d2dd87f1be850cf330e7dd392
JuanCarlos-A <juancar2805@gmail.com>
2024-03-19 11:36:54 -0500

    Numbers in Python

---
+++ python_course/tipos_basicos/06-numeros.py
@@ -0,0 +1,32 @@
+numeroInt = 2
+numeroFloat = 2.5
+numeroImaginario = 2j + 2 # 2j es la unidad imaginaria en Python, 2i igual a la raiz cuadrada de -1
+
+# numeroInt = numeroInt + 2
+numeroInt += 2 # Equivalente a la linea anterior
+
+# numeroInt = numeroInt - 2
+numeroInt -= 2 # Equivalente a la linea anterior
+
+# numeroInt = numeroInt * 2
+numeroInt *= 2 # Equivalente a la linea anterior
+
+# numeroInt = numeroInt / 2
+numeroInt /= 2 # Equivalente a la linea anterior
+
+# numeroInt = numeroInt // 2
+numeroInt //= 2 # Equivalente a la linea anterior
+
+print(1 + 3)
+
+print(1 - 3)
+
+print(1 * 3)
+
+print(1 / 3)
+
+print(1 // 3) # Division entera
+
+print(1 % 3) # Modulo
+
+print(2 ** 3) # Potencia
