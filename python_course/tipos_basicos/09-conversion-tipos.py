2b39fe79893bfb3271596110dc0b908d38b55274
JuanCarlos-A <juancar2805@gmail.com>
2024-03-19 12:17:02 -0500

    Conversor in Python

--- python_course/tipos_basicos/09-conversion-tipos.py
+++ python_course/tipos_basicos/09-conversion-tipos.py
@@ -1,1 +1,12 @@
-x = int(input("Ingresa un numero: "))
+# int()
+# float()
+# str()
+# bool()
+# list()
+
+print(bool(0)) # False
+print(bool(1)) # True
+print(int(2.5)) # 2
+print(float(2)) # 2.0
+print(str(2)) # '2'
+print(list((1, 2, 3))) # [1, 2, 3]
