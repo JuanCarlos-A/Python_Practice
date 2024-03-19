1e0a176aaae5520d11782809705e84fed5048799
JuanCarlos-A <juancar2805@gmail.com>
2024-03-19 11:23:35 -0500

    Format in Python

--- python_course/introduction/format.py
+++ python_course/introduction/format.py
@@ -2,13 +2,12 @@
 
 import dataclasses
 
-CHANCHITO = "feliz"
+CHANCHITO = "feliz" #Representacion de una constante en mayusculas
 A = 12
 B = 13
 
 #Decorador para que automaticamente se generen los metodos de una clase, ejemplo Contructor __init
 @dataclasses.dataclass
-
 class Example:
     """ Class representing a example"""
     a: int
