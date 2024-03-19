cf7292687e75b33bbdda4074eee83bbb8af57d70
JuanCarlos-A <juancar2805@gmail.com>
2024-03-19 11:46:58 -0500

    Modeule math and functions

---
+++ python_course/tipos_basicos/07-numeros-funciones.py
@@ -0,0 +1,21 @@
+import math
+
+print(round(2.5)) # Redondeo
+
+print(abs(-2.5)) # Valor absoluto
+
+print(pow(2, 3)) # Potencia
+
+print(max(1, 2, 3, 4, 5)) # Maximo
+
+print(math.sqrt(9)) # Raiz cuadrada
+
+print(math.pi) # Constante pi
+
+print(math.ceil(2.1)) # Redondeo hacia arriba
+
+print(math.floor(2.9)) # Redondeo hacia abajo
+
+print(math.isnan(2)) # Verifica si el numero es NaN (Not a Number)
+
+print(math.isfinite(2)) # Verifica si el numero es finito
