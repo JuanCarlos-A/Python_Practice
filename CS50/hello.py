909bebcb253790ab40b3fab18f87654162e7a15b
JuanCarlos-A <juancar2805@gmail.com>
2024-03-20 17:41:07 -0500

    Introduction to Programming with python

---
+++ CS50/hello.py
@@ -0,0 +1,3 @@
+name = input("What is your name? ") #Input is a method to get a string from the user. It is a string because it is a text.
+
+print("Hello World ", name) #Print is a method to show a message in the console and Python recognizes the argument as a string.
