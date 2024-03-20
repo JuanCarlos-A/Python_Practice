b053063f4c3049de1b6a19180d1a76544c4bdcfb
JuanCarlos-A <juancar2805@gmail.com>
2024-03-20 17:42:04 -0500

    Flow Control in Python

---
+++ python_course/control_flujo/01-comparadores.py
@@ -0,0 +1,28 @@
+print(1 > 2)
+
+print(1 < 2)
+
+print(1 <= 2)
+
+print(1 >= 2)
+
+print(2 >= 2)
+
+print(2 <= 2) 
+
+print(2 == 2) # The innner part of the statement is a comparison expresion, the outer part is a print statement.
+
+print(2 != 2) # The innner part of the statement is a comparison expresion, the outer part is a print statement.
+
+print("Hello" == "hello")
+
+print(2 == "2") # False because the first 2 is an integer and the second 2 is a string.
+
+print(2 == int("2")) # True because the first 2 is an integer and the second 2 is a string that is converted to an integer.
+
+print(2 == float("2")) # True because the first 2 is an integer and the second 2 is a string that is converted to a float.
+
+print(2 == float("2.0")) # True because the first 2 is an integer and the second 2 is a string that is converted to a float.
+
+print(2 != 2)
+
