029aba7b5d2595dc4a4ac82e665372e813e65ce5
JuanCarlos-A <juancar2805@gmail.com>
2024-03-21 17:47:29 -0500

    Calculator exercise

---
+++ python_course/control_flujo/09-calculadora.py
@@ -0,0 +1,21 @@
+print("Bienvenidos a la calculadora \nPara salir escribe Salir\nLas operaciones son suma, multi, div y resta")
+
+num = None
+
+while True:
+    if num is None:
+        num = int(input("Introduce un número: "))
+    operacion = input("Introduce la operación: ").capitalize().strip()
+    
+    if operacion == "Salir":
+        break
+    elif operacion == "Suma":
+        num += int(input("Introduce otro número: "))
+    elif operacion == "Resta":
+        num -= int(input("Introduce otro número: "))
+    elif operacion == "Multi":
+        num *= int(input("Introduce otro número: "))
+    elif operacion == "Div":
+        num /= int(input("Introduce otro número: "))
+    
+    print(f"El resultado es: {num}")
