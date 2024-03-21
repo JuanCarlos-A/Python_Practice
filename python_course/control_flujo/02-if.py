5d07fa80679d114ebc735182fd8357160687c145
JuanCarlos-A <juancar2805@gmail.com>
2024-03-21 11:35:11 -0500

    Flow Control in Python

--- python_course/control_flujo/02-if.py
+++ python_course/control_flujo/02-if.py
@@ -1,5 +1,9 @@
 edad = 22
-if edad >= 18: #Indicamos al interprete que si la edad es mayor ejecute el bloque de codigo aniadido
-    print("Eres mayor de edad")
+if edad > 54: #Indicamos al interprete que si la edad es mayor ejecute el bloque de codigo aniadido
+    print("Puedes ver la pelicula con descuento")
+elif edad >= 18: #Si la condicion anterior no se cumple, se evalua esta condicion
+    print("Puedes ver la pelicula")
+else: #Si ninguna de las condiciones anteriores se cumple, se ejecuta este bloque de codigo
+    print("No puedes ver la pelicula \n Ve a otro lado")
 
 print("Fin del programa")
