e7919150319fe12983ca11cf6f2720eccb70fa51
JuanCarlos-A <juancar2805@gmail.com>
2024-03-21 16:22:57 -0500

    Loops in Python

---
+++ python_course/control_flujo/07-while.py
@@ -0,0 +1,21 @@
+# FINITOS
+# numero = 1
+
+# while numero < 100:
+#     print(numero)
+#     numero *= 2
+
+
+# comando = ""
+
+# while comando.lower() != "salir":
+#     comando = input("$ ")
+#     print(comando)
+
+# INFINITOS
+
+while True:
+    comando = input("$ ")
+    if comando.lower() == "salir":
+        break
+    print(comando)
