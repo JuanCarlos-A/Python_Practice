2b93459e709a6500f13d6915f9f175ea0327e7c6
JuanCarlos-A <juancar2805@gmail.com>
2024-03-21 11:36:46 -0500

    Ternary operators

---
+++ python_course/control_flujo/03-if-ternario.py
@@ -0,0 +1,21 @@
+#Forma Larga usando el if
+#Primera forma larga
+# edad = 15
+
+# if edad > 17:
+#     print("Es mayor de edad")
+# else:
+#     print("Es menor de edad")
+
+
+
+
+#Formas Cortas unsando el if ternario
+#Primera forma corta
+edad = 15
+edad_comprobada = "Es mayor de edad" if edad > 17 else "Es menor de edad" #Operador ternario
+print(edad_comprobada)
+
+
+#Segunda forma corta
+print("Es mayor de edad") if edad > 17 else print("Es menor de edad")
