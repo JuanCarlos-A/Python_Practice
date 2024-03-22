a3975fda0a94d60bf054a49a9f7eb8efb44694bd
JuanCarlos-A <juancar2805@gmail.com>
2024-03-22 10:57:36 -0500

    Palindrome problem

--- mentorship/leetcode_solution/problem_palindrome.py
+++ mentorship/leetcode_solution/problem_palindrome.py
@@ -1,21 +1,23 @@
+""" Problem: Palindrome Number """
 #Solution 1
 # class Solution:
-#     def isPalindrome(self, x: int) -> bool:
+#     """ Solution Class"""
+#     def is_palindrome(self, x: int) -> bool:
+#        """ is_palindrome Method"""
 #         num_origin = x
 #         num_invert = 0
-
 #         while x > 0:
 #             num = x % 10
-
 #             num_invert = (num_invert * 10) + num
 #             x //= 10
-        
 #         return num_origin == num_invert
 
 
 #Solution 2
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    """ Solution Class"""
+    def is_palindrome(self, x: int) -> bool:
+        """ is_palindrome Method"""
         return str(x)[::-1] == str(x)
-    
-print(Solution().isPalindrome(121))
+
+print(Solution().is_palindrome(121))
