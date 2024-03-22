fa9ed457910528c3f957b9c005ac934d816e6a32
JuanCarlos-A <juancar2805@gmail.com>
2024-03-22 13:59:43 -0500

    Classes in Python

--- mentorship/tasks/classes/vehicle.py
+++ mentorship/tasks/classes/vehicle.py
@@ -1,10 +1,11 @@
 class Vehiculo():
-    def __init__(self, marca, modelo, color, precio):
+    def __init__(self, marca, modelo, color, precio, tipo_motor):
         self.marca = marca
         self.modelo = modelo
         self.color = color
         self.precio = precio
         self.arrancado = False
+        self.tipo_motor = tipo_motor
 
     def __str__(self):
         return f"Marca: {self.marca}\nModelo: {self.modelo}\nColor: {self.color}\nPrecio: {self.precio}\nArrancado: {self.arrancado}"
@@ -17,6 +18,13 @@ class Vehiculo():
         self.arrancado = False
         return "El vehículo ha apagado"
     
+    def tipo(self):
+        return f"Tipo de energía: {self.tipo_motor}"
+    
+    def pico_placa(self):
+        return "Si tiene pico y placa, no puede circular hoy" if self.tipo_motor.lower().strip() == "gasolina" else "No tiene pico y placa, puede circular hoy"
+    
+    
 carro = Vehiculo("Toyota", "Corolla", "Blanco", 20000)
 print(carro)
 print(carro.arrancar())
@@ -28,3 +36,8 @@ print(moto)
 print(moto.arrancar())
 print(moto.apagar())
 print(moto)
+
+
+#Tarea:
+
+#Averiguar _atributo __metodo, static method, getters y setters inheritances, circular importation
