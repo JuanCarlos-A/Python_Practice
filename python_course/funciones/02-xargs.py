3f13c650a03da70cd9ff60ff2ef3c6db288631fe
JuanCarlos-A <juancar2805@gmail.com>
2024-03-22 15:31:11 -0500

    Arguments xargs in Python

--- python_course/funciones/02-xargs.py
+++ python_course/funciones/02-xargs.py
@@ -1,10 +1,20 @@
+""" Args in python """
 #Forma 1
 # def suma(*numeros :float) -> None: # *numeros es una tupla
 #     print(sum(x for x in numeros))  # sum() es una función que suma los elementos de un iterable (lista, tupla, etc)
 
 # suma(2, 5, 10)
 
-def suma(*numeros :float) -> None:
+def suma(*numeros :tuple) -> None: #Estos son xargs o args que reciben los parametros en forma de tupla
+    """
+    Suma los números que se pasen como argumentos, usando los args o xargs
+    Args:
+        *numeros (float): Números a sumar
+    Returns:
+        None
+    
+    Esta función recibe una cantidad indefinida de números y los suma, la cantidad de números que se reciben son una tupla
+    """
     resultado = 0
     for numero in numeros:
         resultado += numero
