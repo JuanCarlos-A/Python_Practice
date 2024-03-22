d1c5753a0ff905029c1a0248e399813b82e8e455
JuanCarlos-A <juancar2805@gmail.com>
2024-03-22 17:56:17 -0500

    Lists in Python

---
+++ python_course/tipos_avanzados/01-listas.py
@@ -0,0 +1,27 @@
+numeros = [1, 2, 3]
+
+flotantes = [1.1, 1.2, 1.2]
+
+palabras = ["Hello", "World", "Python"]
+
+booleanos = [True, False, True]
+
+matriz_listas = [[1, 2, 3], [4, 5, 6]]
+
+matriz_tuplas = [(1, 2, 3), (4, 5, 6)]
+
+matriz_dict = [{"nombre": "Juan", "edad": 20}, {"nombre": "Maria", "edad": 30}]
+
+lista_vacia = []
+
+lista_multi = [0, 1] * 5
+
+sum_listas = numeros + palabras
+
+rango = list(range(1, 11))
+
+palabra = list("Programando ando")
+
+print(matriz_dict[1]["nombre"])
+# a, *b, c = range(5) 
+# print(a, b, c)  # 0 [1, 2, 3] 4 
