5aec5031c54c6797112379c292adfc95f20886d0
JuanCarlos-A <juancar2805@gmail.com>
2024-03-22 11:24:03 -0500

    Functions in Python

--- python_course/funciones/01-funciones.py
+++ python_course/funciones/01-funciones.py
@@ -1,6 +1,8 @@
-def hola(name:str) -> None:
+def hola(first_name:str, last_name:str = "Aguilar") -> None:
     print("Hola Mundo")
     print("Python Course")
-    print(f'Hola {name}')
+    print(f'Hola {first_name} {last_name}')
 
 hola("Juan")
+
+hola(last_name="Perez", first_name="Juan")
