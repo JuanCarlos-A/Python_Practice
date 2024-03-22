72075cadc4dd39db968f9e404c13cd0109f890e6
JuanCarlos-A <juancar2805@gmail.com>
2024-03-22 15:36:43 -0500

    Return in Python

--- python_course/funciones/04-return.py
+++ python_course/funciones/04-return.py
@@ -1,4 +1,13 @@
-def suma(a, b):
+""" Retorno de valores """
+
+def suma(a :int, b: int) -> int:
+    """ Realiza la suma de dos números 
+    Args:
+        a (int): Primer número
+        b (int): Segundo número
+    Returns:
+        int: Suma de los dos números
+    """
     resultado = a + b
     return resultado
 
