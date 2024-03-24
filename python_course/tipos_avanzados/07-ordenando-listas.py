489b6b81049f8b422fa5f3e8c65d154347a495d4
JuanCarlos-A <juancar2805@gmail.com>
2024-03-24 14:05:06 -0500

    Sort Lists  in Python

--- python_course/tipos_avanzados/07-ordenando-listas.py
+++ python_course/tipos_avanzados/07-ordenando-listas.py
@@ -9,15 +9,16 @@ mascotas = [
 
 mascotas.sort(reverse=True, key = lambda x : x[1])
 
-new_list = sorted(mascotas, key=lambda x: x[1])
+new_list = sorted(mascotas, key=lambda x : x[1])
+
 
 
 #Metodo sin sort
 
-def ordena(elemento):
-    return elemento[1]
+# def ordena(elemento):
+#     return elemento[1]
 
-#Aqui sort toma la referencia de la funcion ordena y la usa para ordenar la lista, 
-#pasandole cada elemento de la lista
-mascotas.sort(key=ordena)
+# #Aqui sort toma la referencia de la funcion ordena y la usa para ordenar la lista, 
+# #pasandole cada elemento de la lista
+# mascotas.sort(key=ordena)
 
