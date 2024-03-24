81b70e72c6a4e12473c0c551867251fe1b7cbb49
JuanCarlos-A <juancar2805@gmail.com>
2024-03-24 18:47:17 -0500

    Comprehensions in lists

---
+++ python_course/tipos_avanzados/08-comprehensions-lists.py
@@ -0,0 +1,15 @@
+usuarios = [
+    ["Juan", 35,],
+    ["Ana", 45],
+    ["Pedro", 25],
+    ["Maria", 30],
+    ["Luis", 40]
+]
+
+# List Comprehensions
+
+names = [x[0] for x in usuarios]
+print(names)
+
+users = [x if x[1] % 2 == 0 else False for x in usuarios]
+print(users)
