b6216ea2cb5dde62c317def84a6219e4e167552f
JuanCarlos-A <juancar2805@gmail.com>
2024-03-24 13:36:37 -0500

    Methods of a List

---
+++ python_course/tipos_avanzados/06-agregando-eliminando.py
@@ -0,0 +1,18 @@
+mascotas = [
+    "Pelusa",
+    "Firulais",
+    "Rex",
+    "Milo",
+    "Toby"
+    ]
+
+mascotas.inset(1, "Bobby")
+mascotas.append("Robert")
+
+mascotas.remove("Pelusa")
+mascotas.pop(2) 
+
+del mascotas[3]
+mascotas.clear()
+
+print(mascotas)
