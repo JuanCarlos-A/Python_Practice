2c27f54b8cb176594d7ba2951e2c977755a02ec6
JuanCarlos-A <juancar2805@gmail.com>
2024-03-24 12:03:47 -0500

    Search elements in Python

---
+++ python_course/tipos_avanzados/05-buscar-elemento.py
@@ -0,0 +1,8 @@
+mascotas = ["Pelusa", "Firulais", "Rex", "Milo", "Toby"]
+
+print(mascotas.count("Pelusa"))
+
+print(len(mascotas))
+
+if "Pelusa" in mascotas:
+    print(f'Pelusa esta en el indice {mascotas.index("Pelusa")}')
