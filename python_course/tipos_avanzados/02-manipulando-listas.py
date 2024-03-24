2a2560bdb309519224436167f5dc820bf0865657
JuanCarlos-A <juancar2805@gmail.com>
2024-03-23 21:38:16 -0500

    Manipulating lists in python

---
+++ python_course/tipos_avanzados/02-manipulando-listas.py
@@ -0,0 +1,12 @@
+mascotas = ["Wolfang", "Pelusa", "Pulga", "Copito"]
+print(mascotas[0])
+mascotas[0] = "Firulais"
+
+# print(mascotas)
+# print(mascotas[2:])
+# print(mascotas[-1])
+# print(mascotas[1:2:2])
+
+numeros = list(range(1, 21))
+
+print(numeros)
