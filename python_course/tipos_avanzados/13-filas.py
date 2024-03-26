e60835745e25a5618c72e4eee2871a5b5c6e0bcd
JuanCarlos-A <juancar2805@gmail.com>
2024-03-26 09:49:14 -0500

    Notes in Python

--- python_course/tipos_avanzados/13-filas.py
+++ python_course/tipos_avanzados/13-filas.py
@@ -1,1 +1,17 @@
-lista = [1, 2, 3, 4]
+from collections import deque
+
+fila = deque([1, 2])
+
+print(fila)
+
+fila.append(3)
+
+print(fila)
+
+fila.popleft()
+fila.popleft()
+fila.popleft()
+
+print(fila)
+
+print("La fila está vacía") if not fila else print("La fila tiene elementos")
