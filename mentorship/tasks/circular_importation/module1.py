e71dbfae80c195126385ace3e2e02e481cce9d04
JuanCarlos-A <juancar2805@gmail.com>
2024-03-26 10:59:27 -0500

    Circular_Importation

--- mentorship/tasks/circular_importation/module1.py
+++ mentorship/tasks/circular_importation/module1.py
@@ -2,3 +2,6 @@ import module2
 
 def example():
     module2.another_example()
+
+#La importacion de module2 en module1 y viceversa genera un error de circular importation
+#Para solucionar esto se puede importar el modulo en el metodo que se requiera
--- mentorship/tasks/circular_importation/module2.py
+++ mentorship/tasks/circular_importation/module2.py
@@ -2,3 +2,6 @@ import module1
 
 def another_example():
     module1.example()
+
+
+# Esto puede dar un error de circular importation
