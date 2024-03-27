82b993cfe4e5f5be23cf77f53191854a7da864eb
JuanCarlos-A <juancar2805@gmail.com>
2024-03-26 20:53:37 -0500

    Classes in Python

---
+++ python_course/clases/01-introduccion.py
@@ -0,0 +1,15 @@
+mensaje = "Hola Mundo"
+
+print(type(mensaje))
+
+#Clase: Es el plano de construcción de un objeto
+#Objeto: Es una instancia de una clase
+
+#Clases : El plano de construccion de una casa
+#Objetos : Una casa unica construida a partir de un plano
+
+#Clases : El plano de construccion de un auto
+#Objetos : Un auto unico construido a partir de un plano
+
+#Clases : Humanos
+#Objetos : Juan, Maria, Pedro, etc.
