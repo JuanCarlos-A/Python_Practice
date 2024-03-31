9a3975d6c19271873f98800324843689f2ab8039
JuanCarlos-A <juancar2805@gmail.com>
2024-03-30 19:10:14 -0500

    Constructor in python

--- python_course/clases/03-constructor.py
+++ python_course/clases/03-constructor.py
@@ -1,5 +1,16 @@
 class MiPerro:
-    def habla(self):
-        print("Guau")
+    def __init__(self, nombre :str, edad :float) -> None:
+        self.nombre = nombre
+        self.edad = edad
+        print(f'Se ha creado un perro con el nombre {nombre}')
+              
+    def habla(self) -> None:
+        print(f"{self.nombre} dice: Guau!")
 
-mi_perro = MiPerro()
+mi_perro = MiPerro("Felipe")
+
+mi_perro.habla()
+
+# mi_perro2 = MiPerro("Firulais")
+
+# print(mi_perro2.nombre)
