26ab7064b050a4945224f83f1b50a517a3b9970b
JuanCarlos-A <juancar2805@gmail.com>
2024-04-05 15:19:18 -0500

    Method Resolution Order

--- mentorship/tasks/mro/mro.py
+++ mentorship/tasks/mro/mro.py
@@ -1,19 +1,35 @@
 class PrimeraClase:
-    def funcion(self):
-        print("Metodo de la clase PrimeraClase")
+    pass
 
 class SegundaClase:
-    def funcion(self):
-        print("Metodo de la clase SegundaClase")
+    pass
     
 class TerceraClase(SegundaClase, PrimeraClase):
     pass
 
-class CuartaClase(SegundaClase, TerceraClase):
+class CuartaClase:
+    def funcion(self):
+        print("Metodo de la clase CuartaClase")
+        
+
+class QuintaClase:
+    def funcion(self):
+        print("Metodo de la clase QuintaClase")
+        
+
+class SextaClase(CuartaClase, QuintaClase):
+    def funcion(self):
+        print("Metodo de la clase SextaClase")
+        
+
+class SeptimaClase(TerceraClase, SextaClase):
     pass
+        
 
 # orden de ejecucion [4, 2, 3, 2, 1]
 
-cuarto_objeto = CuartaClase()
+septima = SeptimaClase()
 
-cuarto_objeto.funcion()
+septima.funcion()
+# septima.__mro__()
+print(SeptimaClase.__mro__)
