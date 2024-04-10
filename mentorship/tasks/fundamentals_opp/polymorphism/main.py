43ad6fe92a55c5909f264938c4acd18a1525b23c
JuanCarlos-A <juancar2805@gmail.com>
2024-04-10 10:52:10 -0500

    Polymorphism in python

---
+++ mentorship/tasks/fundamentals_opp/polymorphism/main.py
@@ -0,0 +1,32 @@
+class Perro:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def sonido(self):
+        return "Guau"
+    
+class Gato:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def sonido(self):
+        return "Miau"
+
+def sonido_animal(animal):
+    print(animal.sonido())
+
+perro = Perro("Firulais")
+gato = Gato("Garfield")
+
+sonido_animal(perro)
+sonido_animal(gato)
+
+
+class Persona:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+
+p = Persona("Juan")
+
+print(p)
