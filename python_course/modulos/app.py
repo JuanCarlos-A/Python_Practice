ca49aa79e8284db60730595220eb12cff3321e7a
JuanCarlos-A <juancar2805@gmail.com>
2024-04-11 10:34:32 -0500

    Modules in Python

--- python_course/modulos/app.py
+++ python_course/modulos/app.py
@@ -1,13 +1,14 @@
 from usuarios.impuestos.utilidades import pagar_impuestos
-import usuarios
 
 pagar_impuestos()
 
-print(dir(usuarios))
+# print(__name__)
 
-print(usuarios.__file__)
-print(usuarios.__package__)
-print(usuarios.__path__)
-print(usuarios.__name__)
+# print(dir(usuarios))
 
-print(usuarios.gestion.crud.guardar.__name__)
+# print(usuarios.__file__)
+# print(usuarios.__package__)
+# print(usuarios.__path__)
+# print(usuarios.__name__)
+
+# print(usuarios.gestion.crud.guardar.__name__)
--- python_course/modulos/usuarios/impuestos/utilidades.py
+++ python_course/modulos/usuarios/impuestos/utilidades.py
@@ -1,5 +1,12 @@
-from ..gestion.crud import guardar
+if __name__ != "__main__":
+    # from ..gestion.crud import guardar # Importar modulo de otro paquete de forma relativa
+    from usuarios.gestion.crud import guardar # Importar modulo de otro paquete de forma absoluta
 
-def pagar_impuestos():
-    print("Pagar Impuestos")
-    guardar()
+    def pagar_impuestos():
+        print("Pagar Impuestos")
+        guardar()
+
+    print(__name__)
+
+if __name__ == "__main__":
+    print("Soy el main de utilidades")
