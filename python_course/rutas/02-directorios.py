2df2ee4571808a0188002b5f276f51a0008b1f56
JuanCarlos-A <juancar2805@gmail.com>
2024-04-17 14:05:19 -0500

    Directories in python

--- python_course/rutas/02-directorios.py
+++ python_course/rutas/02-directorios.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-path = Path("rutas")
+path = Path("python_course/rutas")
 
 # path.exists() # Verificar si el directorio existe
 # path.mkdir() # Crear un directorio
@@ -9,3 +9,16 @@ path = Path("rutas")
 
 for p in path.iterdir(): # Iterar sobre los elementos de un directorio
     print(p)
+
+archivos = [p for p in path.iterdir() if not p.is_dir()]
+
+archivos_filtrados = [p for p in path.glob("01*.py")]
+
+archivos_filtrados = [p for p in path.glob("01*.py")]
+
+archivos_filtrados = [p for p in path.glob("01*.py")]
+
+archivos_filtrados = [p for p in path.rglob("**/*.py")]
+
+
+print(archivos_filtrados)
