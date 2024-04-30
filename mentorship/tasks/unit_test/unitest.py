a90936af281335e29c2e436fb8ec8149a350f483
JuanCarlos-A <juancar2805@gmail.com>
2024-04-29 22:28:41 -0500

    Unit Testing

---
+++ mentorship/tasks/unit_test/unitest.py
@@ -0,0 +1,10 @@
+from main import Solution
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    def test_twoSum(self):
+        self.assertEqual(Solution().twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(Solution().twoSum([3, 2, 4], 6), [1, 2])
+        self.assertEqual(Solution().twoSum([3, 3], 6), [0, 1])
+        self.assertEqual(Solution().twoSum([-1, -2, -3, -4, -5], -8), [2, 4])
