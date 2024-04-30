f20b44e863692798a75a29813f2783c5f7550443
JuanCarlos-A <juancar2805@gmail.com>
2024-04-30 15:20:29 -0500

    Pytest

--- mentorship/tasks/unit_test/main.py
+++ mentorship/tasks/unit_test/main.py
@@ -1,9 +1,18 @@
 class Solution():
     def twoSum(self, nums, target):
-        for item in nums:
-            num = nums.index(item)
-            for i in range(num+1, len(nums)):
-                if item + nums[i] == target and num != i:
-                    return [num, i]
+        dicc = {}
+        for index in range(len(nums)):
+            if nums[index] in dicc:
+                print(dicc)
+                return [dicc[nums[index]], index]
+            dicc[target - nums[index]] = index
                     
 # print(Solution().twoSum([-1,-2,-3,-4,-5], -8))
+
+
+# def twoSum(self, nums, target):
+#         for item in nums:
+#             num = nums.index(item)
+#             for i in range(num+1, len(nums)):
+#                 if item + nums[i] == target and num != i:
+#                     return [num, i]
