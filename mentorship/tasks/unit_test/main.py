752290925797aade61e93212917a046ce08b4aa8
JuanCarlos-A <juancar2805@gmail.com>
2024-05-08 12:48:39 -0500

    Unit Testing in Example 1

--- mentorship/tasks/unit_test/main.py
+++
@@ -1,9 +0,0 @@
-class Solution():
-    def twoSum(self, nums, target):
-        dicc = {}
-        for index in range(len(nums)):
-            if nums[index] in dicc:
-                print(dicc)
-                return [dicc[nums[index]], index]
-            dicc[target - nums[index]] = index
-                    
