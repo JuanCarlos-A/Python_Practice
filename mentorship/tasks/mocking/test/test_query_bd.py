6848bf097ba1340cb12fc60c786084e9baba93d5
JuanCarlos-A <juancar2805@gmail.com>
2024-05-08 10:32:48 -0500

    Mocking in Python

---
+++ mentorship/tasks/mocking/test/test_query_bd.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import Mock
+from main import obtener_nombres_usuarios
+
+class QueryBD(unittest.TestCase):
+
+    def test_query_nombres(self):
+
+        db_mock = Mock()
+
+        db_mock.query.return_value = [
+            {'id': 1, 'nombre': 'Alice'},
+            {'id': 2, 'nombre': 'Bob'}
+        ]
+
+        nombres = obtener_nombres_usuarios(db_mock)
+        self.assertEqual(nombres, ['Alice', 'Bob'])
+
+
+if __name__ == '__main__':
+    unittest.main()
