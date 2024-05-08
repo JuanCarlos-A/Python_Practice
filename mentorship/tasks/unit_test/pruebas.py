7c7af423c5a35d13315058bf5ded328bc8805e4b
JuanCarlos-A <juancar2805@gmail.com>
2024-05-07 19:38:49 -0500

    Pruebas Unitarias en Python

--- mentorship/tasks/unit_test/pruebas.py
+++
@@ -1,7 +0,0 @@
-from main import Solution
-
-def test_twoSum():
-    """ Unit Test of function twoSum """
-    assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
-    assert Solution().twoSum([3, 2, 4], 6) == [1, 2]
-    assert Solution().twoSum([3, 3], 6) == [0, 0]
