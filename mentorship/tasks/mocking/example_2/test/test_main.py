fb7acd0685ca6ba748a25070ccee86e63b303f80
JuanCarlos-A <juancar2805@gmail.com>
2024-05-09 21:34:19 -0500

    Mocking in Python

--- mentorship/tasks/mocking/example_2/test/test_main.py
+++ mentorship/tasks/mocking/example_2/test/test_main.py
@@ -3,20 +3,22 @@ from unittest.mock import Mock
 
 class TestExample(unittest.TestCase):
 
-    json = Mock()
+    def test_mock_function(self):
+
+        json = Mock()
 
 
-    json.loads(s='{"k" : "v"}')
+        json.loads(s='{"k" : "v"}')
 
-    json.loads.assert_called()
-    json.loads.assert_called_once()
-    json.loads.assert_called_with(s='{"k" : "v"}')
-    json.loads.assert_called_once_with(s='{"k" : "v"}')
+        json.loads.assert_called()
+        json.loads.assert_called_once()
+        json.loads.assert_called_with(s='{"k" : "v"}')
+        json.loads.assert_called_once_with(s='{"k" : "v"}')
 
-    json.loads('{"k" : "v"}')
+        json.loads('{"k" : "v"}')
 
-    json.loads.assert_called_with('{"k" : "v"}')
-    # json.loads.assert_not_called()
+        json.loads.assert_called_with('{"k" : "v"}')
+        # json.loads.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
